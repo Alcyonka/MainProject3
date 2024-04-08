d75d857cfda3676e55cf1c5da1416aac27a7d4f7
A M <Alcyonespace@gmail.com>
2024-04-08 21:35:10 +0300

    it is working!

--- client1/src/App.js
+++ client1/src/App.js
@@ -1,7 +1,27 @@
 import TextEditor from "./TextEditor"
 
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate
+} from 'react-router-dom'
+import { v4 as uuidV4 } from 'uuid'
+
 function App() {
-    return <TextEditor />
+    const eprst=`/documents/${uuidV4()}`;
+    return (
+    <Router>
+        <Routes>
+            <Route path="/" element={<Navigate to={`/documents/${uuidV4()}`} />} />
+            
+            <Route path="/documents/:id" element ={<TextEditor />} />
+            
+
+        </Routes>
+      
+    </Router>
+    )
 }
 
 export default App
